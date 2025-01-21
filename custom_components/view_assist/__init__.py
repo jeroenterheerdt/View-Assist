d134605d2e68faf929c3628d7248739da714560e
Donny F <donny911@hotmail.com>
2025-01-21 14:34:56 -0600

    Add a temporary service
    Adding a temporary service to use as a base for useful ones

--- custom_components/view_assist/__init__.py
+++ custom_components/view_assist/__init__.py
@@ -1,6 +1,11 @@
 from homeassistant.config_entries import ConfigEntry
 from homeassistant.const import Platform
-from homeassistant.core import HomeAssistant
+from homeassistant.core import (
+    HomeAssistant,
+    ServiceCall,
+    ServiceResponse,
+    SupportsResponse,
+)
 from .const import DOMAIN
 
 PLATFORMS: list[Platform] = [Platform.SENSOR]
@@ -10,8 +15,22 @@ async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry):
     hass.data.setdefault(DOMAIN, {})
     # Request platform setup
     await hass.config_entries.async_forward_entry_setups(entry, PLATFORMS)
-    return True
 
+    async def handle_fuel_location_lookup(call: ServiceCall) -> ServiceResponse:
+        """Handle a fuel location lookup call."""
+        radius = call.data.get("location")
+        lat = call.data.get("location")
+        return {"radius": radius, "lat": lat}
+
+    hass.services.async_register(
+        DOMAIN,
+        "view_assist",
+        handle_fuel_location_lookup,
+        supports_response=SupportsResponse.ONLY,
+    )
+
+    return True
+    
 async def async_unload_entry(hass: HomeAssistant, entry: ConfigEntry):
     """Unload a config entry."""
     if unloaded := await hass.config_entries.async_forward_entry_unload(entry, PLATFORMS):
