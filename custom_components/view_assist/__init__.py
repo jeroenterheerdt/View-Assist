9830dedd356c3f555a9492a10e7c39ac1a2b234d
Donny F <donny911@hotmail.com>
2025-01-21 18:58:48 -0600

    More work learning to expand services

--- custom_components/view_assist/__init__.py
+++ custom_components/view_assist/__init__.py
@@ -7,6 +7,10 @@ from homeassistant.core import (
     SupportsResponse,
 )
 from .const import DOMAIN
+import homeassistant.helpers.entity_registry as er
+import logging
+
+_LOGGER = logging.getLogger(__name__)
 
 PLATFORMS: list[Platform] = [Platform.SENSOR]
 
@@ -16,16 +20,42 @@ async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry):
     # Request platform setup
     await hass.config_entries.async_forward_entry_setups(entry, PLATFORMS)
 
-    async def handle_fuel_location_lookup(call: ServiceCall) -> ServiceResponse:
-        """Handle a fuel location lookup call."""
-        radius = call.data.get("location")
-        lat = call.data.get("location")
-        return {"radius": radius, "lat": lat}
+
+    async def handle_get_id(call: ServiceCall) -> ServiceResponse:
+        """Handle a device lookup call."""
+        hass: HomeAssistantType = call.hass
+        entity_registry = er.async_get(hass)
+        entity_id = call.data.get('entity_id')
+        
+        entity_entry = entity_registry.async_get(entity_id)
+        if entity_entry and entity_entry.device_id:
+            device_id = entity_entry.device_id
+            return {"device_id": device_id}
+        
+        return {"error":  "Device ID not found"}
+
+        # entity_id = call.data.get('entity_id')
+        # state = hass.states.get(entity_id)
+        # mic_device = state.attributes.get('mic_device')
+        # display_device = state.attributes.get('display_device')
+        # return {"mic_device": mic_device, "display_device": display_device}
 
     hass.services.async_register(
         DOMAIN,
-        "view_assist",
-        handle_fuel_location_lookup,
+        "get_id",
+        handle_get_id,
+        supports_response=SupportsResponse.ONLY,
+    )
+
+    async def handle_satellite_lookup(call: ServiceCall) -> ServiceResponse:
+        """Handle a satellite lookup call."""
+        device_id = call.data.get("device_id")
+        return {"device_id_received": device_id}
+
+    hass.services.async_register(
+        DOMAIN,
+        "get_satellite",
+        handle_satellite_lookup,
         supports_response=SupportsResponse.ONLY,
     )
 
